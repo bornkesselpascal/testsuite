b799c30f15b9ab8bfad58e2b295451029b262732
Pascal <89606160+bornkesselpascal@users.noreply.github.com>
2023-10-31 07:25:26 +0100

    Improvements to the definition of the parameters for custom_tester

--- src/custom_tester.cpp
+++ src/custom_tester.cpp
@@ -6,6 +6,9 @@
 #include <unistd.h>
 
 const long long int loss_limit = 50;
+const int queue_frequency = 1000;
+const bool queue_log = true;
+const bool use_qos = false;
 
 //                    _ _         _
 //                 __| (_)___ _ _| |_
@@ -15,7 +18,7 @@ const long long int loss_limit = 50;
 
 custom_tester_client::custom_tester_client(custom_tester_client_description description)
     : m_description(description)
-    , m_comm_client(m_description.server_ip, m_description.port, m_description.qos)
+    , m_comm_client(m_description.server_ip, m_description.port, use_qos)
     , m_comm_server(m_description.client_ip, (m_description.port + 1))
 {
 }
@@ -74,24 +77,27 @@ void custom_tester_client::run(struct test_results::custom* results) {
             m_comm_client.send(data, msg_size);
             msg_counter++;
 
-            if((msg_counter % 100000) == 0) {
+            if((msg_counter % queue_frequency) == 0) {
                 m_comm_client.send(&query_message, sizeof(query_message));
 
                 int bytes_received = m_comm_server.receive(&query_results, sizeof(query_results), true);
                 if(bytes_received == sizeof(query_results)) {
                     query_diff = msg_counter - query_results.number_received;
-                    clock_gettime(CLOCK_MONOTONIC, &current_time);
-                    if ((current_time.tv_nsec - start_time.tv_nsec) < 0) {
-                        result_time.tv_sec = current_time.tv_sec - start_time.tv_sec - 1;
-                        result_time.tv_nsec = current_time.tv_nsec - start_time.tv_nsec + 1000000000L;
-                    } else {
-                        result_time.tv_sec = current_time.tv_sec - start_time.tv_sec;
-                        result_time.tv_nsec = current_time.tv_nsec - start_time.tv_nsec;
-                    }
-                    results->query_response.push_back({query_diff, msg_counter, (result_time.tv_sec + (result_time.tv_nsec * 1e-9))});
                     msg_counter++;
 
-                    if((msg_counter > 5000000) && (query_diff > loss_limit)) {
+                    if(queue_log) {
+                        clock_gettime(CLOCK_MONOTONIC, &current_time);
+                        if ((current_time.tv_nsec - start_time.tv_nsec) < 0) {
+                            result_time.tv_sec = current_time.tv_sec - start_time.tv_sec - 1;
+                            result_time.tv_nsec = current_time.tv_nsec - start_time.tv_nsec + 1000000000L;
+                        } else {
+                            result_time.tv_sec = current_time.tv_sec - start_time.tv_sec;
+                            result_time.tv_nsec = current_time.tv_nsec - start_time.tv_nsec;
+                        }
+                        results->query_response.push_back({query_diff, msg_counter, (result_time.tv_sec + (result_time.tv_nsec * 1e-9))});
+                    }
+
+                    if(query_diff > loss_limit) {
                         std::cout << "[ERROR] Loss limit reached. Aborting test scenario." << query_diff << std::endl;
                         break;
                     }
@@ -154,7 +160,7 @@ void custom_tester_client::run(struct test_results::custom* results) {
 custom_tester_server::custom_tester_server(custom_tester_server_description description)
     : m_description(description)
     , m_comm_server(m_description.server_ip, m_description.port)
-    , m_comm_client(m_description.client_ip, (m_description.port + 1), m_description.qos)
+    , m_comm_client(m_description.client_ip, (m_description.port + 1), use_qos)
 {
 }
 
