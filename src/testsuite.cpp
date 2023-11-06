7a8d6b09ec9d65c7a52dcfb51a7e79b8dc9f20bf
Pascal <89606160+bornkesselpascal@users.noreply.github.com>
2023-11-06 11:50:37 +0100

    Updated TestSuite for star topology
    - one version for server and client
    - support for classic and parallel mode
    - support for the parallel execution of multiple test campaigns

--- src/testsuite.cpp
+++ src/testsuite.cpp
@@ -6,49 +6,187 @@
 #include "test_control_client.h"
 #include "test_control_server.h"
 
-void stress_example();
+#include <iostream>
+#include <fstream>
+#include <string>
 
-int main() {
-    std::string file_name = "/testsuite/master_list.xml";
+#ifndef COMMIT_HASH
+#define COMMIT_HASH "0000000"
+#endif
 
-    std::vector<std::string> filepaths = test_control_parser::read_client_main_XML(file_name);
+enum mode
+{
+    CLASSIC,
+    PARALLEL
+};
+enum type
+{
+    CLIENT,
+    SERVER
+};
 
-    for (const std::string& current_path : filepaths) {
-        client_description current_description = test_control_parser::read_client_from_XML(current_path);
+bool get_user_input(type &type, mode &mode);
+void start_client(std::string path);
+void start_server(std::string path);
+void parallel_execution(type type, std::vector<std::string> filepaths);
 
-        test_control_client current_client(current_description);
-        current_client.run();
+int main()
+{
+    std::cout << "TestSuite (v" << COMMIT_HASH << ")" << std::endl
+              << std::endl;
+
+    type type;
+    mode mode;
+    if (!get_user_input(type, mode))
+    {
+        return 1;
     }
 
-    return 0;
+    switch (type)
+    {
+    case CLIENT:
+    {
+        std::string filename = "/testsuite/client_master.xml";
+        std::vector<std::string> filepaths = test_control_parser::read_client_main_XML(filename);
 
-//std::string filename = "/home/aidass/Developer/TestSuite/server_description.xml";
-//server_description mdesc = test_control_parser::read_server_from_XML(filename);
-//test_control_server mserv(mdesc);
-//mserv.run();
+        switch (mode)
+        {
+        case CLASSIC:
+        {
+            if (filepaths.size() > 0)
+            {
+                start_client(filepaths[0]);
+            }
+            break;
+        }
+        case PARALLEL:
+        {
+            parallel_execution(type, filepaths);
+            break;
+        }
+        }
 
+        break;
+    }
+    case SERVER:
+    {
+        std::string filename = "/testsuite/server_master.xml";
+        std::vector<std::string> filepaths = test_control_parser::read_server_main_XML(filename);
 
-//    custom_tester_client client_test({"10.30.0.1", "10.30.0.2", 8100, 100000, {65500, false}, 30});
-//    //custom_tester_server server_test({"10.30.0.1", "10.30.0.2", 8100, {10000}});
+        switch (mode)
+        {
+        case CLASSIC:
+        {
+            if (filepaths.size() > 0)
+            {
+                start_server(filepaths[0]);
+            }
+            break;
+        }
+        case PARALLEL:
+        {
+            parallel_execution(type, filepaths);
+            break;
+        }
+        }
 
-//    struct test_results results;
+        break;
+    }
+    }
+}
 
-//    client_test.run(&(results.custom));
-//    //server_test.run(&(results.custom));
+bool get_user_input(type &type, mode &mode)
+{
+    // ASK FOR CLIENT OR SERVER
+    std::cout << "Do you want to start a Client or Server? (c/s): ";
+    char input;
+    std::cin >> input;
+    if (input == 'c')
+    {
+        type = CLIENT;
+    }
+    else if (input == 's')
+    {
+        type = SERVER;
+    }
+    else
+    {
+        return false;
+    }
 
-//    test_results_parser::write_to_XML("/home/aidass/Downloads/results.xml", results, test_description::metadata::CUSTOM);
+    // ASK FOR MODE (CLASSIC OR PARALLEL)
+    std::cout << "Do you want to start in Classic or Parallel mode? (c/p): ";
+    std::cin >> input;
+    if (input == 'c')
+    {
+        mode = CLASSIC;
+    }
+    else if (input == 'p')
+    {
+        mode = PARALLEL;
+    }
+    else
+    {
+        return false;
+    }
 
+    return true;
+}
 
-//    return 0;
+void start_client(std::string path)
+{
+    int process_id = getpid();
+    std::cout << "Launched client... [" << std::to_string(process_id) << "][" << path << "]" << std::endl;
 
-//client_description m_description = {
-//    test_description::metadata::CUSTOM, "/testsuite/custom_test_eno1", {40, 60}, {"10.30.0.1", "10.30.0.2", "", 8100, 100000, {{500, 1450, 8900, 65000}, false}}, {"enp1s0f0", "enp1s0f0"}, {"10.30.0.2", 8080}, false, {test_description::stress::CPU_USR, {1, 16, 3}, test_description::stress::LOC_BOTH}
-//};
+    client_description current_description = test_control_parser::read_client_from_XML(path);
+    test_control_client current_client(current_description);
+    current_client.run();
+}
 
-//test_control_parser::write_client_to_XML("/testsuite/tc_client_files/custom_example.xml", m_description);
+void start_server(std::string path)
+{
+    int process_id = getpid();
+    std::cout << "Launched server... [" << std::to_string(process_id) << "][" << path << "]" << std::endl;
 
+    server_description current_description = test_control_parser::read_server_from_XML(path);
+    test_control_server current_server(current_description);
+    current_server.run();
+}
 
-
-
-
+void parallel_execution(type type, std::vector<std::string> filepaths)
+{
+    // Iterate through all client descriptions with index i
+    for (int i = 0; i < filepaths.size(); i++)
+    {
+        switch (fork())
+        {
+        case -1:
+        {
+            // Error
+            exit(1);
+        }
+        case 0:
+        {
+            switch (type)
+            {
+            case CLIENT:
+            {
+                start_client(filepaths[i]);
+                break;
+            }
+            case SERVER:
+            {
+                start_server(filepaths[i]);
+                break;
+            }
+            }
+            exit(0);
+        }
+        default:
+        {
+            // Parent
+            break;
+        }
+        }
+    }
 }
