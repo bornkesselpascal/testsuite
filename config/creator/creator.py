17835480fbd64acead087d2f5d50e2a9d3b0d8a5
Pascal <89606160+bornkesselpascal@users.noreply.github.com>
2023-11-07 11:46:25 +0100

    Added the creation of server xml files for config_creator

--- config/creator/creator.py
+++ config/creator/creator.py
@@ -1,62 +1,9 @@
-import xml.etree.ElementTree as ET
+import os
 import re
+from support.xmlcreator import create_client_xml, create_server_xml
 
-test_name = "newcard_inspur"
+test_name = "first_test"
 
-
-def create_client_xml(client_name, client_ip, client_port, client_interface, server_ip, server_port, server_interface):
-    # Create the root element
-    root = ET.Element("client_description")
-    
-    # Create 'method' element
-    ET.SubElement(root, "method").text = "CUSTOM"
-
-    # Create 'path' element
-    ET.SubElement(root, "path").text = f"/testsuite/{client_name}/{test_name}"
-
-    # Create 'duration' element
-    duration = ET.SubElement(root, "duration")
-    ET.SubElement(duration, "short_duration").text = "7200"
-    ET.SubElement(duration, "long_duration").text = "0"
-
-    # Update 'target_connection' with provided client IP and port
-    target_connection = ET.SubElement(root, "target_connection")
-    ET.SubElement(target_connection, "client_ip").text = client_ip
-    ET.SubElement(target_connection, "server_ip").text = server_ip  # server IP is now dynamic
-    ET.SubElement(target_connection, "bandwidth_limit").text = ""
-    ET.SubElement(target_connection, "port").text = client_port
-    ET.SubElement(target_connection, "gap").text = "2500"
-
-    # Create 'datagram' element
-    datagram = ET.SubElement(target_connection, "datagram")
-    sizes = ET.SubElement(datagram, "sizes")
-    for size in ["80", "8900", "65000"]:
-        ET.SubElement(sizes, "value").text = size
-    ET.SubElement(datagram, "random").text = "false"
-
-    # Create 'interface' element
-    interface = ET.SubElement(root, "interface")
-    ET.SubElement(interface, "client").text = client_interface
-    ET.SubElement(interface, "server").text = server_interface
-
-    # Update 'service_connection' with provided server IP and port
-    service_connection = ET.SubElement(root, "service_connection")
-    ET.SubElement(service_connection, "server_ip").text = server_ip  # server IP is now dynamic
-    ET.SubElement(service_connection, "port").text = server_port
-   
-    # Create 'client_only' element
-    ET.SubElement(root, "client_only").text = "false"
-
-    # Create 'stress' element
-    stress = ET.SubElement(root, "stress")
-    ET.SubElement(stress, "type").text = "NONE"
-    ET.SubElement(stress, "location").text = "LOC_BOTH"
-
-    # Create the XML tree and write it to a file
-    tree = ET.ElementTree(root)
-    if hasattr(ET, 'indent'):
-        ET.indent(tree, space="\t", level=0)  # Indent for pretty print, for Python 3.9 and later
-    tree.write(f"client/{client_name}.xml", encoding="utf-8", xml_declaration=True)
 
 def process_input_file(input_file):
     clients = {}
@@ -75,13 +22,20 @@ def process_input_file(input_file):
                 if 'client' in name:
                     clients[identifier] = (name, ip, port, interface)
                 elif 'server' in name:
-                    servers[identifier] = (ip, port, interface)
+                    servers[identifier] = (name, ip, port, interface)
+
+    # Create client and server directories if not already present
+    if not os.path.exists('client'):
+        os.makedirs('client')
+    if not os.path.exists('server'):
+        os.makedirs('server')
 
     # Now create an XML file for each client
     for identifier, (client_name, client_ip, client_port, client_interface) in clients.items():
         if identifier in servers:
-            server_ip, server_port, server_interface = servers[identifier]
-            create_client_xml(client_name, client_ip, client_port, client_interface, server_ip, server_port, server_interface)
+            server_name, server_ip, server_port, server_interface = servers[identifier]
+            create_client_xml(test_name, client_name, client_ip, client_port, client_interface, server_ip, server_port, server_interface)
+            create_server_xml(server_name, server_ip, server_port)
 
 # Specify the path to your input file here
 input_file = 'pairs.txt'
---
+++ config/creator/support/xmlcreator.py
@@ -0,0 +1,74 @@
+import xml.etree.ElementTree as ET
+
+def create_client_xml(test_name, client_name, client_ip, client_port, client_interface, server_ip, server_port, server_interface):
+    # Create the root element
+    root = ET.Element("client_description")
+    
+    # Create 'method' element
+    ET.SubElement(root, "method").text = "CUSTOM"
+
+    # Create 'path' element
+    ET.SubElement(root, "path").text = f"/testsuite/{client_name}/{test_name}"
+
+    # Create 'duration' element
+    duration = ET.SubElement(root, "duration")
+    ET.SubElement(duration, "short_duration").text = "7200"
+    ET.SubElement(duration, "long_duration").text = "0"
+
+    # Update 'target_connection' with provided client IP and port
+    target_connection = ET.SubElement(root, "target_connection")
+    ET.SubElement(target_connection, "client_ip").text = client_ip
+    ET.SubElement(target_connection, "server_ip").text = server_ip  # server IP is now dynamic
+    ET.SubElement(target_connection, "bandwidth_limit").text = ""
+    ET.SubElement(target_connection, "port").text = client_port
+    ET.SubElement(target_connection, "gap").text = "2500"
+
+    # Create 'datagram' element
+    datagram = ET.SubElement(target_connection, "datagram")
+    sizes = ET.SubElement(datagram, "sizes")
+    for size in ["80", "8900", "65000"]:
+        ET.SubElement(sizes, "value").text = size
+    ET.SubElement(datagram, "random").text = "false"
+
+    # Create 'interface' element
+    interface = ET.SubElement(root, "interface")
+    ET.SubElement(interface, "client").text = client_interface
+    ET.SubElement(interface, "server").text = server_interface
+
+    # Update 'service_connection' with provided server IP and port
+    service_connection = ET.SubElement(root, "service_connection")
+    ET.SubElement(service_connection, "server_ip").text = server_ip  # server IP is now dynamic
+    ET.SubElement(service_connection, "port").text = server_port
+   
+    # Create 'client_only' element
+    ET.SubElement(root, "client_only").text = "false"
+
+    # Create 'stress' element
+    stress = ET.SubElement(root, "stress")
+    ET.SubElement(stress, "type").text = "NONE"
+    ET.SubElement(stress, "location").text = "LOC_BOTH"
+
+    # Create the XML tree and write it to a file
+    tree = ET.ElementTree(root)
+    if hasattr(ET, 'indent'):
+        ET.indent(tree, space="\t", level=0)  # Indent for pretty print, for Python 3.9 and later
+    tree.write(f"client/{client_name}.xml", encoding="utf-8", xml_declaration=True)
+
+
+def create_server_xml(server_name, server_ip, server_port):
+    # Create the root element
+    root = ET.Element("server_description")
+
+    # Create 'path' element
+    ET.SubElement(root, "path").text = f"/testsuite"
+
+    # Update 'service_connection' with provided server IP and port
+    service_connection = ET.SubElement(root, "service_connection")
+    ET.SubElement(service_connection, "server_ip").text = server_ip
+    ET.SubElement(service_connection, "port").text = server_port
+
+    # Create the XML tree and write it to a file
+    tree = ET.ElementTree(root)
+    if hasattr(ET, 'indent'):
+        ET.indent(tree, space="\t", level=0)  # Indent for pretty print, for Python 3.9 and later
+    tree.write(f"server/{server_name}.xml", encoding="utf-8", xml_declaration=True)
