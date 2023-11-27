a22d6c9853eba36baea60d7c9bfda9d8c177f5dd
Pascal <89606160+bornkesselpascal@users.noreply.github.com>
2023-11-27 11:34:51 +0100

    Updated config for iHawk center tests

--- config/creator/parameters.py
+++ config/creator/parameters.py
@@ -1,8 +1,6 @@
 # Parameters
 
-test_name      = "first_test"
-test_duration  = "3600"
-test_cycletime = "2500"
-test_datagrams = ["80",
-                  "8900",
-                  "65000",]
+test_name      = "ihwk_95p"
+test_duration  = "1200"
+test_cycletime = "54000"
+test_datagrams = ["65000"]
