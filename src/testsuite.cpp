dbc68a0766ca0227269105ee12c032b2308e00be
Pascal <89606160+bornkesselpascal@users.noreply.github.com>
2023-11-27 11:38:15 +0100

    Added CPU affinity to TestSuite

--- src/testsuite.cpp
+++ src/testsuite.cpp
@@ -23,11 +23,17 @@ enum type
     CLIENT,
     SERVER
 };
+enum affinity
+{
+    DISABLED,
+    ENABLED,
+    INVERSED
+};
 
-bool get_user_input(type &type, mode &mode, bool &realtime);
-void start_client(std::string path);
-void start_server(std::string path);
-void parallel_execution(type type, std::vector<std::string> filepaths);
+bool get_user_input(type &type, mode &mode, bool &realtime, affinity &affinity);
+void start_client(std::string path, affinity affinity);
+void start_server(std::string path, affinity affinity);
+void parallel_execution(type type, std::vector<std::string> filepaths, affinity affinity);
 
 int main()
 {
@@ -42,7 +48,8 @@ int main()
     type type;
     mode mode;
     bool realtime;
-    if (!get_user_input(type, mode, realtime))
+    affinity affinity;
+    if (!get_user_input(type, mode, realtime, affinity))
     {
         std::cerr << std::endl << "[error][coordinator#2] Invalid user input." << std::endl;
         return 1;
@@ -73,13 +80,13 @@ int main()
         {
             if (filepaths.size() > 0)
             {
-                start_client(filepaths[0]);
+                start_client(filepaths[0], affinity);
             }
             break;
         }
         case PARALLEL:
         {
-            parallel_execution(type, filepaths);
+            parallel_execution(type, filepaths, affinity);
             break;
         }
         }
@@ -97,13 +104,13 @@ int main()
         {
             if (filepaths.size() > 0)
             {
-                start_server(filepaths[0]);
+                start_server(filepaths[0], affinity);
             }
             break;
         }
         case PARALLEL:
         {
-            parallel_execution(type, filepaths);
+            parallel_execution(type, filepaths, affinity);
             break;
         }
         }
@@ -119,7 +126,7 @@ int main()
     return 0;
 }
 
-bool get_user_input(type &type, mode &mode, bool &realtime)
+bool get_user_input(type &type, mode &mode, bool &realtime, affinity &affinity)
 {
     // ASK FOR CLIENT OR SERVER
     std::cout << "Do you want to start a Client or Server? (c/s): ";
@@ -170,30 +177,108 @@ bool get_user_input(type &type, mode &mode, bool &realtime)
         return false;
     }
 
+    // ASK FOR AFFINITY
+    std::cout << "Do you want to enable cpu affinity? (d/e/i): ";
+    std::cin >> input;
+    if (input == 'd')
+    {
+        affinity = DISABLED;
+    }
+    else if (input == 'e')
+    {
+        affinity = ENABLED;
+    }
+    else if (input == 'i')
+    {
+        affinity = INVERSED;
+    }
+    else
+    {
+        return false;
+    }
+
     return true;
 }
 
-void start_client(std::string path)
-{
+void start_client(std::string path, affinity affinity)
+{   
     int process_id = getpid();
-    std::cout << "Launched client... [" << std::to_string(process_id) << "][" << path << "]" << std::endl;
+
+    std::string cores = "0-32";
+    if(affinity != DISABLED) {
+        if((path.find('1') != std::string::npos) || (path.find('4') != std::string::npos)) {
+            if(affinity == ENABLED) {
+                cores = "8-15,24-31"; // Socket 2
+            }
+            else if (affinity == INVERSED) {
+                cores = "0-7,16-23"; // Socket 1
+            }
+        }
+        else if((path.find('2') != std::string::npos) || (path.find('3') != std::string::npos)) {
+            if(affinity == ENABLED) {
+                cores = "0-7,16-23"; // Socket 1
+            }
+            else if (affinity == INVERSED) {
+                cores = "8-15,24-31"; // Socket 2
+            }
+        }
+        else {
+            std::cerr << "[error][coordinator#3] Could not determine cores." << std::endl;
+            return;
+        }
+
+        if(0 != system(("taskset -cp " + cores + " " + std::to_string(process_id)).c_str())) {
+            return;
+        }
+    }
+
+    std::cout << "Launched client... [" << std::to_string(process_id) << "][" << path << "] on Cores " << cores << std::endl;
 
     client_description current_description = test_control_parser::read_client_from_XML(path);
     test_control_client current_client(current_description);
     current_client.run();
 }
 
-void start_server(std::string path)
+void start_server(std::string path, affinity affinity)
 {
     int process_id = getpid();
-    std::cout << "Launched server... [" << std::to_string(process_id) << "][" << path << "]" << std::endl;
+
+    std::string cores = "0-32";
+    if(affinity != DISABLED) {
+        if((path.find('1') != std::string::npos) || (path.find('4') != std::string::npos)) {
+            if(affinity == ENABLED) {
+                cores = "8-15,24-31"; // Socket 2
+            }
+            else if (affinity == INVERSED) {
+                cores = "0-7,16-23"; // Socket 1
+            }
+        }
+        else if((path.find('2') != std::string::npos) || (path.find('3') != std::string::npos)) {
+            if(affinity == ENABLED) {
+                cores = "0-7,16-23"; // Socket 1
+            }
+            else if (affinity == INVERSED) {
+                cores = "8-15,24-31"; // Socket 2
+            }
+        }
+        else {
+            std::cerr << "[error][coordinator#3] Could not determine cores." << std::endl;
+            return;
+        }
+
+        if(0 != system(("taskset -cp " + cores + " " + std::to_string(process_id)).c_str())) {
+            return;
+        }
+    }
+
+    std::cout << "Launched server... [" << std::to_string(process_id) << "][" << path << "] on Cores " << cores << std::endl;
 
     server_description current_description = test_control_parser::read_server_from_XML(path);
     test_control_server current_server(current_description);
     current_server.run();
 }
 
-void parallel_execution(type type, std::vector<std::string> filepaths)
+void parallel_execution(type type, std::vector<std::string> filepaths, affinity affinity)
 {
     // Iterate through all client descriptions with index i
     for (int i = 0; i < filepaths.size(); i++)
@@ -211,12 +296,12 @@ void parallel_execution(type type, std::vector<std::string> filepaths)
             {
             case CLIENT:
             {
-                start_client(filepaths[i]);
+                start_client(filepaths[i], affinity);
                 break;
             }
             case SERVER:
             {
-                start_server(filepaths[i]);
+                start_server(filepaths[i], affinity);
                 break;
             }
             }
