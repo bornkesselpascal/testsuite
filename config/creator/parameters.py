070f072a2350c1ced61d66778a2af8aa969bf44d
Pascal <89606160+bornkesselpascal@users.noreply.github.com>
2023-12-11 12:36:04 +0100

    Updated config files

--- config/creator/parameters.py
+++ config/creator/parameters.py
@@ -1,16 +1,16 @@
 # Parameters
 
-test_name      = "ihwk_7B"
-test_duration  = "90"
+test_name      = "ihwk_A2"
+test_duration  = "100"
 
 test_dynamic   = True
-test_dyn_mode  = "CYCLE_TIME"     # DISABLED, DATAGRAM_SIZE, CYCLE_TIME
-test_dyn_min   = "1"
-test_dyn_max   = "301"
-test_dyn_steps = "15"
+test_dyn_mode  = "DATAGRAM_SIZE"     # DISABLED, DATAGRAM_SIZE, CYCLE_TIME
+test_dyn_min   = "9080"
+test_dyn_max   = "64080"
+test_dyn_steps = "10"
 
 test_type      = "ST_UDP"       # ST_UDP, ST_RAW, ST_PACKET
 test_cycletime = "1"
-test_datagrams = ["65000"]
+test_datagrams = ["80"]
 
 test_latency   = "END_TO_END"   # DISABLED, END_TO_END, FULL
